fae774a5dcbfc118bb9d3368786cb17933c8b70a
Jeff Wolski <jeffersonwolski@gmail.com>
2013-03-14 15:14:52 -0500

    adds push delete stub
    Signed-off-by: Jeff Wolski <jeffersonwolski@gmail.com>

---
+++ client-library/model/PushDeleteResonse.h
@@ -0,0 +1,13 @@
+//
+//  PushDeleteResonse.h
+//  Percero
+//
+//  Created by Jeff Wolski on 3/13/13.
+//
+//
+
+#import "SyncResponse.h"
+
+@interface PushDeleteResonse : SyncResponse
+
+@end
---
+++ client-library/model/PushDeleteResonse.m
@@ -0,0 +1,13 @@
+//
+//  PushDeleteResonse.m
+//  Percero
+//
+//  Created by Jeff Wolski on 3/13/13.
+//
+//
+
+#import "PushDeleteResonse.h"
+
+@implementation PushDeleteResonse
+
+@end
