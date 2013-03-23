f6df152a2017cf7ac19b6846423682e0d1c7ca5a
Jeff Wolski <jeffersonwolski@gmail.com>
2013-03-23 15:44:21 -0500

    retool and implement relationship methods
    Signed-off-by: Jeff Wolski <jeffersonwolski@gmail.com>

--- client-library/PFSourceRelationship.m
+++ client-library/PFSourceRelationship.m
@@ -10,4 +10,24 @@
 
 @implementation PFSourceRelationship
 
+- (id)initBidirectionalWithPropertyName:(NSString *)propertyName
+                       inverseClassName:(NSString *)inverseClassName
+                    inversePropertyName:(NSString *)inversePropertyName{
+    self = [super init];
+    if (self) {
+        self.propertyName = propertyName;
+        self.inverseClassName = inverseClassName;
+        self.inversePropertyName = inversePropertyName;
+    }
+    return self;
+}
+
+- (id)initUnidirectionalWithPropertyName:(NSString *)propertyName{
+    self = [super init];
+    if (self) {
+        self.propertyName = propertyName;
+    }
+    return self;
+}
+
 @end
--- client-library/PFTargetRelationship.m
+++ client-library/PFTargetRelationship.m
@@ -11,12 +11,28 @@
 @implementation PFTargetRelationship
 
 
-- (id)initUnidirectionalWithInverseClassName:(NSString *)inverseClassName inversePropertyName:(NSString *)inversePropertyName{
-    if ([super init]) {
+- (id)initUnidirectionalWithInverseClassName:(NSString *)inverseClassName inversePropertyName:(NSString *)inversePropertyName isRequired:(BOOL)isRequired{
+    self = [super init];
+    if (self) {
         self.inverseClassName = inverseClassName;
         self.inversePropertyName = inversePropertyName;
         
+        _isRequired = isRequired;
     }
     return self;
+}
+
+- (id)initBidirectionalWithPropertyName:(NSString *)propertyName inverseClassName:(NSString *)inverseClassName inversePropertyName:(NSString *)inversePropertyName isRequired:(BOOL)isRequired{
+    self = [super init];
+    if (self) {
+        
+        self.propertyName = propertyName;
+        self.inverseClassName = inverseClassName;
+        self.inversePropertyName = inversePropertyName;
+        
+        _isRequired = isRequired;
+    }
+    return self;
+
 }
 @end
