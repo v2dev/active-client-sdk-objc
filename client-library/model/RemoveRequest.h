5256537a84b95a29d7fb443ecd0add79b2ba4f3d
Jeff Wolski <jeffersonwolski@gmail.com>
2013-03-26 09:41:59 -0500

    fixed class comment
    Signed-off-by: Jeff Wolski <jeffersonwolski@gmail.com>

--- client-library/model/RemoveRequest.h
+++ client-library/model/RemoveRequest.h
@@ -4,7 +4,7 @@
 //
 //  Created by Jeff Wolski on 3/26/13.
 //
-//  The client will use this class to initiate the deletion of an object from the system.
+//  Use this class to initiate the deletion of an object from the system.
 
 #import "SyncRequest.h"
 #import "ClassIDPair.h"
