e0b042216bbd981bb6023b66b35695ec9a74eb56
Jeff Wolski <jeffersonwolski@gmail.com>
2013-07-28 22:14:42 -0500

    add JsonObject Category to PFModelObject
    adds a jsonData method that takes the toDictionary output
    and returns NSData representation from NSJSONDeserializer
    
    Signed-off-by: Jeff Wolski <jeffersonwolski@gmail.com>

--- client-library/JsonObject.h
+++ client-library/JsonObject.h
@@ -8,12 +8,17 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
-
+#import "PFModelObject.h"
 
 @interface JsonObject : NSManagedObject
 
 @property (nonatomic, retain) NSData * jsonData;
 @property (nonatomic, retain) NSString * nameOfClass;
 @property (nonatomic, retain) NSString * objectId;
+
+@end
+
+@interface PFModelObject (JsonObject)
+- (NSData *)jsonData;
 
 @end
--- client-library/JsonObject.m
+++ client-library/JsonObject.m
@@ -8,11 +8,21 @@
 
 #import "JsonObject.h"
 
-
 @implementation JsonObject
 
 @dynamic jsonData;
 @dynamic nameOfClass;
 @dynamic objectId;
+
+@end
+
+@implementation PFModelObject (JsonObject)
+
+- (NSData *)jsonData;
+{
+    NSError *errorJSON = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:[self toDictionary:self.isShell] options:NSJSONWritingPrettyPrinted error:&errorJSON];
+    return jsonData;
+}
 
 @end
