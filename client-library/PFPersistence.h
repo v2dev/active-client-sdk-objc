f0dec78b19c31e2f20f08b6865d3d7fcd3be29ea
Jeff Wolski <jeffersonwolski@gmail.com>
2013-08-26 23:23:26 -0500

    add save persistent method calls to persistence header
    Signed-off-by: Jeff Wolski <jeffersonwolski@gmail.com>

--- client-library/PFPersistence.h
+++ client-library/PFPersistence.h
@@ -7,11 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
-
+@class PFModelObject;
 @interface PFPersistence : NSObject
 
 + (void)addObject:(id) object;
 
 + (void) sendFindByIdRequestWithRemoteClassName:(NSString*)remoteClassName objectId:(NSString*)objectId callBackTarget:(NSObject*) target callbackMethod:(SEL) selector;
++ (void) sendCreateRequestWithClass:(NSString*)className object:(PFModelObject *) object completionTarget:(NSObject*) target method:(SEL) selector;
++ (void) sendPutRequestWithClass:(NSString*)className object:(PFModelObject *) object completionTarget:(NSObject*) target method:(SEL) selector;
 + (void) setup;
 @end
